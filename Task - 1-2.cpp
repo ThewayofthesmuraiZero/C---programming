6534b8c728f195b7cae71d4aad632e4bdfad5c44
Kristina <krissty87@mail.ru>
2021-04-07 15:37:01 +0300

    Задание 1-1
    Создать консольное приложение, вычисляющее значения переменных
    по представленным в таблице формулам. Расчёт примера осуществить
    по заданным константам. Вывести на экран значения исходных данных,
    а также результат вычислений. Дополнить свой отчёт блок-схемой алгоритма
    ВАРИАНТ 11
    
    Задание 1-2
    
    Создать консольное приложение для решения задачи, представленной
    в таблице. Данные для решения вводит пользователь. Помните, что ввод
    необходимо проверять на правильность (только числа). Вывести результат
    вычислений на экран. При необходимости дополнить свой отчёт поясняющими
    формулами, помогающими решить задачу. Дополнить свой отчёт блок-схемой
    алгоритма.
     ВАИРАНТ 11
    Даны два числа. Найти среднее арифметическое
    кубов этих чисел и среднее геометрическое модулей этих чисел.

---
+++ Task - 1-2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RUSSIAN");
+    double a;
+    double b;
+    cout << "Введите два числа";
+    cout << "a="; cin >> a;
+    cout << "b="; cin >> b;
+
+    cout << (a * a * a + b * b * b)/2 << "  " << sqrt(fabs(a * b)) << "\n";
+
+}
+
---
+++ Task 1-1.cpp
@@ -0,0 +1,20 @@
+﻿#define USE_MATH_DEFINES 
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int Square (int length);
+double Divide (double devident, double devider);
+bool isEven(int value);
+
+int main()
+{
+    setlocale(LC_ALL, "RUSSIAN");
+    double x = 2.2;
+    double y = 9.2;
+    double z = 10.2;
+    const auto a = (log(2.718281) * (z + x * x) + sin((x / y) * sin((x/y))));
+    const auto b = exp(-z)*((x + sqrt(x + z))/(x - sqrt(abs(x - y))));
+
+    cout << a << ' ' << b;
+}
