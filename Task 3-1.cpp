a621041790fccb2e7d62b7fdcf6af4b5ca61d2ec
Kristina <krissty87@mail.ru>
2021-04-07 16:22:28 +0300

    Задание 3-1
    Протабулировать заданную в таблице функцию.
    Использовать данные в таблице значения шага и интервала в качестве
    ввода пользователя для решения тестового примера.
    При невозможности расчёта функции в конкретной точке выводить
    её значение и надпись, означающую отсутствие решения.

---
+++ Task 3-1.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <limits>
+
+using namespace std;
+void  IsCalulted(const double x, double y);
+void  y(const double x, const double y);
+
+int main()
+{
+	setlocale(LC_ALL, "RUSSIAN");
+
+	const auto leftBound = 1.0;
+	const auto rightBound = 0.5;
+	const auto step = 0.1;
+
+	auto x = leftBound;
+	cout << "x             y \n";
+
+	while ((x < leftBound) || (abs(x - rightBound) < step))
+	{
+		double y;
+		if ( IsCalulted(x, y))
+		{
+			cout << " x = " << setprecision(2) << x
+				<< "y = " << setprecision(5) << y << endl;
+			x += step;
+		}
+		else
+		{
+			cout << " x = " << setprecision(2) << x
+				<< " not defined\n";
+		}
+
+	}
+
+
+
+	void  IsCalulted(const double x, double y);
+	{
+		const auto IsCalulated = x >= 0;
+	}
+	
+	void y(const double x, const double y);
+	{
+	    return x + cos(pow(x, 0.52) + 2);
+	}
+	return 0;
+
+}
