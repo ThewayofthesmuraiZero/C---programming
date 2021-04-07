993a70424f1fea87c2971344dac2a05b3c136724
Kristina <krissty87@mail.ru>
2021-04-07 16:02:13 +0300

    Задание 2-1
    ВАРИАНТ 11
    Создать консольное приложение для решения задачи, представленной
    в таблице, с использованием перечислимого типа. Выбор формулы вычисления
     зависит от пользователя. Данные для решения задачи так же вводит
    пользователь. Ввод необходимо проверять на правильность.
    Все результаты вывести на экран. Дополнить свой отчёт блок-схемой
    алгоритма.
    
    ЗАДАНИЕ 2-2
    ВАРИАНТ 11
    Создать консольное приложение с организацией разветвлённого процесса
    для решения заданной в таблице функции. Данные для решения вводит
    пользователь. Ввод необходимо проверять на правильность. Все результаты
     вывести на экран. Дополнить свой отчёт блок-схемой алгоритма.

---
+++ Task 2-1.cpp
@@ -0,0 +1,40 @@
+﻿
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    setlocale(LC_ALL, "RUSSIAN");
+
+    double a, v, sp, sg;
+    int b;
+
+    cout << " Введите грань: \n";
+    cin >> a;
+
+    cout << " Выберете что хотите посчитать\n";
+    cout << " 1 - Объем \n";
+    cout << " 2 - Площадь полной поверхности \n";
+    cout << " 3 - Площадь грани \n";
+
+    cin >> b;
+
+    switch (b)
+    {
+        case 1:
+            v = a * a * a;
+            cout << " Объем =" << v << " \n";
+            break;
+
+        case 2:
+            sp = 6 * a * a;
+            cout << " Площадь полной поверхности = " << sp << " \n";
+            break;
+
+        case 3:
+            sg = a * a;
+            cout << " Площадь грани = " << sg << " \n";
+            break;
+    }
+ return 0;
+}
---
+++ Task 2-2.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <limits>
+
+using namespace std;
+
+int main() 
+{
+
+    setlocale(LC_ALL, "RUSSIAN");
+    
+    const double a = 0.3;
+
+    double x;
+    cout << "Введите переменную х = ";
+    cin >> x;
+    double result;
+
+    if ((x * a) < 1)
+    {
+        result = a* sin((x * x - 1) / 10);
+        cout << "При условии ((x * a) < 1) => y = " << setprecision(15) << result;
+    }
+    else ((x * a) >= 1);
+    {
+        result = a* cos((x - 1) / 10);
+        cout << "При условии ((x * a) => 1) => y = " << setprecision(15) << result;
+    }
+    return(0);
+}
