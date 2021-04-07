19cb6b3eb65cdda75fa3ff3842fb7bec13aaf42b
Kristina <krissty87@mail.ru>
2021-04-07 18:18:16 +0300

    Задание 1-3
    Создать консольное приложение для решения задачи, представленной
    в таблице. Данные для решения вводит пользователь. Помните, что ввод
    необходимо проверять на правильность (только числа). Вывести результат
    вычислений на экран. При необходимости дополнить свой отчёт поясняющими
     формулами, помогающими решить задачу. Дополнить свой отчёт блок-схемой
     алгоритма.
    ВАРИАНТ 11
    Чему равна потенциальная энергия пружины, растянутой на x мм, если
    коэффициент её жёсткости равен k Н/м?

---
+++ Task 1-3.cpp
@@ -0,0 +1,35 @@
+﻿#define USE_MATH_DEFINES
+#include <cmath>
+#include <iostream>
+
+
+double ConvertStretching(const double metr);
+double Stiffnesscoefficient(const double power, const double metr);
+
+
+int main()
+{
+	setlocale(LC_ALL, "RUSSIAN");
+	double Stretching, Stiffnesscoefficient;
+	std::cout << " Введите размер растяжения пружины в мм и коэффициент жесткости в Н/м, чтобы найти потенциальную энергию пружины ";
+    std::cin >> Stretching >> Stiffnesscoefficient;
+
+	const auto metr = ConvertStretching(Stretchin);
+	const auto  energy = fnesscoefficient(const double power, const double metr);
+
+	std::cout << "Потенциальная энергия пружины равна " << energy << ;
+		
+	return 0;
+}
+
+
+double ConvertStretching(const double metr);
+{ 
+	const auto mm = 1000;
+	return mm * metr;
+}
+
+double Stiffnesscoefficient(const double power, const double metr);
+{
+	return (mm * pow(mm, 2)) / 2;
+}
