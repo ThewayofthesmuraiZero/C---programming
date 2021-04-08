cc56d48c6d1cecd3edc21da95d9da469c9a67f3e
ThewayofthesmuraiZero <70698521+ThewayofthesmuraiZero@users.noreply.github.com>
2021-04-08 14:49:59 +0300

    Update Task - 1-2.cpp

--- Task - 1-2.cpp
+++ Task - 1-2.cpp
@@ -1,10 +1,20 @@
-﻿#include <iostream>
+// see https://docs.microsoft.com/ru-ru/cpp/c-runtime-library/math-constants?view=msvc-160
+#define _USE_MATH_DEFINES // for C++
+
+#include <iostream>
+#include <iomanip>
 #include <cmath>
+
 using namespace std;
+/**
+ * \brief Точка входа в программу.
+ * \return Код ошибки (0 - успех).
+ */
 
 int main()
 {
     setlocale(LC_ALL, "RUSSIAN");
+    
     double a;
     double b;
     cout << "Введите два числа";
