b13314e1e1bd208f4fc055b62cae8edd7a5156d3
ThewayofthesmuraiZero <70698521+ThewayofthesmuraiZero@users.noreply.github.com>
2021-04-08 16:14:40 +0300

    Update Task 1-3.cpp

--- Task 1-3.cpp
+++ Task 1-3.cpp
@@ -1,35 +1,45 @@
-﻿#define USE_MATH_DEFINES
+  
+#define _USE_MATH_DEFINES // for C++
 #include <cmath>
 #include <iostream>
 
 
-double ConvertStretching(const double metr);
-double Stiffnesscoefficient(const double power, const double metr);
-
+double ConvertToMetrs(const double metr);
+ddouble GetEnergy(const double stiffness, const double displacement);
 
 int main()
 {
-	setlocale(LC_ALL, "RUSSIAN");
-	double Stretching, Stiffnesscoefficient;
-	std::cout << " Введите размер растяжения пружины в мм и коэффициент жесткости в Н/м, чтобы найти потенциальную энергию пружины ";
-    std::cin >> Stretching >> Stiffnesscoefficient;
+    double metrmm, displacement;
+    std::cout << "Введите размер растяжения пружины в мм и коэффициент жесткости в Н/м, чтобы найти потенциальную энергию пружины\n";
+    std::cin >> metrmm >> displacement;
 
-	const auto metr = ConvertStretching(Stretchin);
-	const auto  energy = fnesscoefficient(const double power, const double metr);
+    const auto metrmm = ConvertToMetrs(metrmm);
+    const auto energy = GetEnergy(metrmm, displacement);
 
-	std::cout << "Потенциальная энергия пружины равна " << energy << ;
-		
-	return 0;
+    std::cout << "Потенциальная энергия пружины равна " << energy;
+    return 0;
 }
 
 
-double ConvertStretching(const double metr);
-{ 
-	const auto mm = 1000;
-	return mm * metr;
-}
-
-double Stiffnesscoefficient(const double power, const double metr);
+double ConvertToMetrs(const double metr);
 {
-	return (mm * pow(mm, 2)) / 2;
+    const auto mm = 1000;
+	return mm / metr;
 }
+/**
+ * \brief Метод перевода миллиметров в метры
+ * \mm - миллиметры 
+ * \return миллиметры в метры
+ */
+
+double GetEnergy(const double stiffness, const double displacement);
+{
+    return (displacement * pow(displacement, 2)) / 2;
+}
+
+/**
+ *   \brief Метод расчёта потенциальногй энергии в СИ.
+ *
+ *   \param metr метры (кг).
+ *   \param displacement коэффициент жесткости (Н/м)
+ */
