0adc3b847954a1d898df55cfe1ca41dc61ef1bb1
Kristina <krissty87@mail.ru>
2021-04-10 11:57:36 +0300

    ЗАДАЧА 2-3
    Создать консольное приложение с организацией разветвлённого процесса
    для решения задачи из таблицы. Данные для решения вводит пользователь.
    Ввод необходимо проверять на правильность.
    Все результаты вывести на экран. Дополнить свой отчёт блок-схемой
    алгоритма.
    
    ВАРИАНТ 11
    Даны три положительных числа. Определить, можно ли построить
    треугольник с длинами сторон, равным этим числам.
    Если можно, то ответить на вопрос, является ли он остроугольным
    
    Я проверяю 2 условия:
    1) Треугольник строится или нет
    2) Треугольник является остроугольным

---
+++ TASK 2-3.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+/**
+ * \brief
+ * Функция определения является ли треугольник остроугольным?
+ * \param a целое число
+ * \param b целое число
+ * \param c целое число
+ * \return true при условии, что треугольник возможно построить, после является ли он остроугольным
+ */
+bool TriangleRules(const int a, const int b, const int c);
+bool OstrTreangle(const int a, const int b, const int c);
+/**
+ * \brief Точка входа в программу
+ * \return код ошибки. 0 - успех
+ */
+int main()
+{
+    setlocale(LC_ALL, "RUSSIAN");
+
+    cout << "Введите 3 числа, чтобы построить треугольник\n";
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    const auto result = TriangleRules(a, b, c)
+        ? "Треугольник возможно построить"
+        : "Треугольник нельзя построить ";
+    
+    const auto result2 = OstrTreangle(a, b, c)
+        ? "Треугольник остроугольный"
+        : "Треугольник не остроугольный ";  
+
+    cout << result;
+    cout << result2;
+
+    return 0;
+}
+
+bool TriangleRules(const int a, const int b, const int c)
+{
+    return     (a + b > c || a + c > b || b + c > a) == (a + b < c || a + c < b || b + c < a) ;
+}
+
+bool OstrTreangle(const int a, const int b, const int c)
+{
+    return ((a * a + b * b = c * c) || (a * a + c * c = b * b) || (b * b + c * c = a * a) == (a * a + b * b != c * c) || (a * a + c * c != b * b) || (b * b + c * c != a * a));
+}
+
+///  (a* a + b * b = c * c) || (a * a + c * c = b * b) || (b * b + c * c = a * a);
+///  (a + b > c || a + c > b || b + c > a) == (a + b < c || a + c < b || b + c < a);
