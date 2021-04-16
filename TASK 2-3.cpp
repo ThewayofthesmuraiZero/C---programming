37d81da648c18d7330f2de97970c753fd9e949ff
ThewayofthesmuraiZero <70698521+ThewayofthesmuraiZero@users.noreply.github.com>
2021-04-16 11:50:09 +0300

    Update Task 2-3
    Задача 2-3 внесла изменения.

--- TASK 2-3.cpp
+++ TASK 2-3.cpp
@@ -2,17 +2,22 @@
 #include <cmath>
 
 using namespace std;
-
 /**
  * \brief
- * Функция определения является ли треугольник остроугольным?
- * \param a целое число
- * \param b целое число
- * \param c целое число
- * \return true при условии, что треугольник возможно построить, после является ли он остроугольным
+ * \ Определить является ли треугольник остроугольным?
+ * \param a - первая сторона стреугольника
+ * \param b - вторая сторона стреугольника
+ * \param c - вторая сторона стреугольника
+ * \return true при условии, что треугольник возможно построить, после ответить ли он остроугольным?
  */
-bool TriangleRules(const int a, const int b, const int c);
-bool OstrTreangle(const int a, const int b, const int c);
+bool isTriangleExists(const double a, const double b, const double c);
+/**
+ * \brief
+ * Возможно ли построить треугольник
+ * \TriangleRules - Правила существования треуголника\
+ */
+
+
 /**
  * \brief Точка входа в программу
  * \return код ошибки. 0 - успех
@@ -20,34 +25,18 @@ bool OstrTreangle(const int a, const int b, const int c);
 int main()
 {
     setlocale(LC_ALL, "RUSSIAN");
-
     cout << "Введите 3 числа, чтобы построить треугольник\n";
-    int a, b, c;
+    double a, b, c;
     cin >> a >> b >> c;
-
-    const auto result = TriangleRules(a, b, c)
+    const auto result = isTriangleExists(a, b, c)
         ? "Треугольник возможно построить"
-        : "Треугольник нельзя построить ";
-    
-    const auto result2 = OstrTreangle(a, b, c)
-        ? "Треугольник остроугольный"
-        : "Треугольник не остроугольный ";  
-
+        : "Треугольник остроугольный";
     cout << result;
-    cout << result2;
-
     return 0;
 }
 
-bool TriangleRules(const int a, const int b, const int c)
+bool isTriangleExists(const double a, const double b, const double c)
 {
-    return     (a + b > c || a + c > b || b + c > a) == (a + b < c || a + c < b || b + c < a) ;
+    return (a + b > c || a + c > b || b + c > a) == ((a * a + b * b = c * c) || (a * a + c * c = b * b) || (b * b + c * c = a * a) ;
 }
 
-bool OstrTreangle(const int a, const int b, const int c)
-{
-    return ((a * a + b * b = c * c) || (a * a + c * c = b * b) || (b * b + c * c = a * a) == (a * a + b * b != c * c) || (a * a + c * c != b * b) || (b * b + c * c != a * a));
-}
-
-///  (a* a + b * b = c * c) || (a * a + c * c = b * b) || (b * b + c * c = a * a);
-///  (a + b > c || a + c > b || b + c > a) == (a + b < c || a + c < b || b + c < a);
