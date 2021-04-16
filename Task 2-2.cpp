1256099ba6ddb72d3b8516eeca50c1dcaa9641b6
ThewayofthesmuraiZero <70698521+ThewayofthesmuraiZero@users.noreply.github.com>
2021-04-16 10:39:02 +0300

    UP Task 2-2

--- Task 2-2.cpp
+++ Task 2-2.cpp
@@ -1,24 +1,44 @@
-﻿#include <iostream>
+﻿
+#define _USE_MATH_DEFINES // for C++
+#include <iostream>
 #include <cmath>
 #include <iomanip>
 #include <limits>
 
 using namespace std;
 
+/**
+ * \brief Функция расчета для случая, когда x*а <= 1.
+ * \const double x Входная величина.
+ * \const double a Входная величина.
+ * \return Значение функции.
+ */
+
 double f1(const double x, const double a);
+
+/**
+ * \brief Функция расчета для случая, когда x <= a.
+ * \param x Входная величина.
+ * \param a Входная величина.
+ * \return Значение функции.
+ */
+
 double f2(const double x, const double a);
 
+/**
+ * \brief Точка входа в программу.
+ * \return Код ошибки (0 - успех).
+ */
 
 int main() 
 {
 
     setlocale(LC_ALL, "RUSSIAN");
-    
        const auto a = 0.3;
     double x;
     cout << "Введите переменную x = ";
     cin >> x;
-    if (x*a< 1)
+    if ( x * a < 1 )
     {
         cout << "y = " << f1(x, a);
     }
@@ -32,10 +52,10 @@ int main()
 
 double f1(const double x, const double a)
 {
-    return a* sin((x * x - 1) / 10);
+    return a * sin((x * x - 1) / 10);
 }
 
 double f2(const double x, const double a)
 {
-    returna* cos((x - 1) / 10);
+    return a * cos((x - 1) / 10);
 }
